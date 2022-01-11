c83d17b3062790b2af0be9b85fa967c4e7e99705
Oluwatobi Immanuel <agbeleyeoluwatobi@gmail.com>
2022-01-11 10:03:05 +0100

    made cha

--- auth/src/server.ts
+++ auth/src/server.ts
@@ -1,11 +1,24 @@
 import app from './config/app'
 import colors from 'colors'
-// import { seedData } from './config/seeds/seeder.seed'
-// import connectDB from './config/db'
+import { seedData } from './config/seeds/seeder.seed'
+import connectDB from './config/db'
+import { getMemoryStats, getHeapSize } from './utils/memory.util'
 
 const connect = async (): Promise<void> => {
 
+    // connect to DB
+    await connectDB();
+
+    // get heap statistics and log heap size
+    const heapSize = getMemoryStats()
+    console.log(heapSize);
+
+    // seed data
+    await seedData();
+
 }
+
+connect();  // initialize connection and seed data
 
 // define PORT
 const PORT = process.env.PORT || 5000;
