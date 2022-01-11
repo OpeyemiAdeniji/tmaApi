5bf0397c5f45facfa1b634ec22218efc5b97b119
Oluwatobi Immanuel <agbeleyeoluwatobi@gmail.com>
2022-01-11 08:20:16 +0100

    made changes: fixed

---
+++ common/src/middleware/auth.mw.ts
@@ -0,0 +1,80 @@
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import { Request } from 'express'
+import { getRolesByName } from './role.mw'
+import { ObjectId } from 'mongoose'
+
+export const protect = (req: Request, secret: string): string | JwtPayload => {
+
+    let result: string | JwtPayload = '';
+    let token: string = '';
+
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+
+        token = req.headers.authorization.split(' ')[1];  // get token from bearer
+
+    }else if(req.cookies.token){
+
+        token = req.cookies.token;
+
+    }
+
+    if(!token || token === ''){
+        result = ''
+    }
+
+    if(token){
+
+        const jwtData = jwt.verify(token, secret);
+        result = jwtData;
+
+    }
+
+    return result;
+
+}
+
+export const authorize = async (roles: Array<string>, userRoles: Array<string>, authType: string): Promise<boolean> => {
+
+    let allRoles: any = [];
+    let resultFlag: boolean = false;
+
+    await getRolesByName(roles, authType).then((resp) => {
+        allRoles = [...resp];
+    });
+
+    // get authorized IDs
+    const ids = allRoles.map((e: any) => { return e._id });
+
+    // check if user roles matches the authorized roles
+    const flag = await checkRole(ids, userRoles);
+
+    if(flag){
+        resultFlag = true
+    }else{
+        resultFlag = false;
+    }
+
+    return resultFlag;
+
+
+}
+
+const checkRole = (roleIds: Array<string>, roles: Array<string>): boolean => {
+
+    let flag: boolean = false;
+
+    for(let i = 0; i < roleIds.length; i++){
+
+        for(let j = 0; j < roles.length; j++){
+
+            if(roleIds[i].toString() === roles[j].toString()){
+                flag = true;
+            }
+
+        }
+
+    }
+
+    return flag;
+
+}
---
+++ common/src/middleware/db.mw.ts
@@ -0,0 +1,62 @@
+import mongoose, { Model, Schema } from 'mongoose';
+import colors from 'colors'
+
+const MONGODB_URI = 'mongodb+srv://Immanuel:WJUFgQQCSf94LTkZ@checkaam.hxnke.mongodb.net/tma-auth-db?retryWrites=true&w=majority'
+const MONGODB_PROD_URI = 'mongodb+srv://Immanuel:WJUFgQQCSf94LTkZ@checkaam.hxnke.mongodb.net/tma-authp-db?retryWrites=true&w=majority'
+const MONGODB_CLOUD_URI = 'mongodb+srv://Immanuel:vs8w16K2a4YfR530@cluster-btf-e2823008.mongo.ondigitalocean.com/tma-authp-db?authSource=admin&replicaSet=cluster-btf'
+let dbConn: any = null; //use global var
+
+const options: object = {
+
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    autoIndex: true,
+    keepAlive: true,
+    maxPoolSize: 10,
+    // bufferMaxEntries: 0,
+    wtimeoutMS:2500,
+    connectTimeoutMS: 10000,
+    socketTimeoutMS: 45000,
+    family: 4,
+    // useFindAndModify: false,
+    useUnifiedTopology: true
+
+}
+
+const cert = `${__dirname.split('middleware')[0]}src/ca-certificate.crt`;
+const cloudDBString = MONGODB_CLOUD_URI + `&tls=true&tlsCAFile=${cert}`
+// console.log(__dirname);
+
+export const connectDB = (authType: string): void => {
+
+    if(authType === 'development'){
+
+        dbConn = mongoose.createConnection(MONGODB_URI, options);
+        // console.log('Auth (dev) database connected');
+
+    }else if(authType === 'production'){
+
+        // vs8w16K2a4YfR530
+        dbConn = mongoose.createConnection(MONGODB_PROD_URI, options);
+        // console.log('Auth (prod) database connected');
+
+    }else if(authType === 'cloud'){
+
+        // vs8w16K2a4YfR530
+        dbConn = mongoose.createConnection(cloudDBString, options);
+        // console.log('Auth (prod) database connected');
+
+    }else {
+        console.log('Authentication type is required');
+    }
+
+}
+
+export const getRoleModel = async (authType: string): Model<Schema> => {
+
+    await connectDB(authType);
+    
+    const model = await dbConn.collection('roles');
+    return model;
+
+}
