4bb77ec5edf95475cab033be2a6a063601e71229
Oluwatobi Immanuel <agbeleyeoluwatobi@gmail.com>
2022-01-22 19:05:46 +0100

    ...

--- common/build/events/subjects.ev.d.ts
+++
@@ -1,7 +0,0 @@
-export declare enum Subjects {
-    UserCreated = "tma.user.created",
-    CountryFound = "tma.country.found",
-    LocationSaved = "tma.location.saved",
-    NotificationCreated = "tma.notification.created",
-    TalentApplied = "tma.talent.applied"
-}
