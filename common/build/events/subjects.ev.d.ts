eeb0ed2be41ac1906349a18e5f2ef0ff4cd4096f
Oluwatobi Immanuel <agbeleyeoluwatobi@gmail.com>
2022-01-26 15:39:36 +0100

    ..

---
+++ common/build/events/subjects.ev.d.ts
@@ -0,0 +1,8 @@
+export declare enum Subjects {
+    UserCreated = "tma.user.created",
+    CountryFound = "tma.country.found",
+    LocationSaved = "tma.location.saved",
+    NotificationCreated = "tma.notification.created",
+    TalentApplied = "tma.talent.applied",
+    TalentAdded = "tma.talent.added"
+}
---
+++ common/build/events/subjects.ev.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Subjects = void 0;
+// subject enums
+var Subjects;
+(function (Subjects) {
+    Subjects["UserCreated"] = "tma.user.created";
+    Subjects["CountryFound"] = "tma.country.found";
+    Subjects["LocationSaved"] = "tma.location.saved";
+    Subjects["NotificationCreated"] = "tma.notification.created";
+    Subjects["TalentApplied"] = "tma.talent.applied";
+    Subjects["TalentAdded"] = "tma.talent.added";
+})(Subjects = exports.Subjects || (exports.Subjects = {}));
